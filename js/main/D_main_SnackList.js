8e3dc10b1873e77ec04b856d45e9bc3949b8e380
mandoo98 <minijjan@naver.com>
2023-07-23 20:36:56 +0900

    오류 1차 수정

--- js/main/D_main_SnackList.js
+++ js/main/D_main_SnackList.js
@@ -18,11 +18,11 @@ const createItem = (product)=>{
     const img = document.createElement('img');
     const p = document.createElement('p');
 
-    img.setAttribute('src',product.img);
-    img.classList = 'item_img';
-
     a.setAttribute('href',product.href);
     a.classList = 'item_link';
+
+    img.setAttribute('src',product.img);
+    img.classList = 'item_img';
     
     li.id = product.id;
     li.classList = 'D_items';
